a71208957c5dcb2c4b14a66afc0d609442006b96
Garbanv <garban.valdeon@gmail.com>
2020-05-20 20:52:36 +0200

    exercise 1 completed

---
+++ js/main.js
@@ -0,0 +1,58 @@
+function getData(){
+    
+    const tbody = document.querySelector('#tbody');
+    fetch('../json/list.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+      return  data.map(function(type){
+             const tdTitle = document.createElement("td");
+             const tdHref = document.createElement("td");
+             const tdDelete = document.createElement("td");
+             const tdEdit = document.createElement("td");
+ 
+             const hrefLink = document.createElement("a");
+            //  const formLink = document.createElement("a");
+             const editLink = document.createElement("a");
+             
+             const tr = document.createElement("tr");
+
+             const trashIcon = document.createElement("i");
+             const editIcon = document.createElement("i");
+
+            trashIcon.className ="far fa-trash-alt";
+            editIcon.className ="far fa-edit";
+
+            tdTitle.innerText=type.title;
+            tdDelete.appendChild(trashIcon)
+            tdHref.appendChild(hrefLink);
+            hrefLink.setAttribute('href', `${type.href}`)
+            editLink.setAttribute('href',`http://127.0.0.1:5500/edit/${type.id}`)
+            tdEdit.appendChild(editLink)
+            tdEdit.className = "text-center"
+            tdDelete.className = "text-center"    
+            hrefLink.innerText=type.href;
+            // formLink.setAttribute('href', `http://127.0.0.1:5500/${type.id}`)
+            // formLink.appendChild(trashIcon);
+            editLink.appendChild(editIcon);
+            
+            tr.appendChild(tdTitle);
+            tr.appendChild(tdHref);
+            tr.appendChild(tdEdit);
+            tr.appendChild(tdDelete);
+            
+            trashIcon.addEventListener("click", removeItem)
+
+            tbody.appendChild(tr);
+        })
+    })
+    .catch(err => console.log(err))
+}
+
+
+function removeItem(){
+    const buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+}
+
+getData()
